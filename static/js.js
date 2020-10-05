d3f5fb402f00e25a2cd1e7fe782778cba8b45a18
Lillyoverton <Lillyoverton1@gmail.com>
2020-10-05 21:54:23 +1300

    edit js.js

--- static/js.js
+++ static/js.js
@@ -1,4 +1,9 @@
 // load birthdays
-
 fetch('/nzjanuary', { method: 'GET' })
   .then(response => console.log(response))
+
+
+  document.getElementById('squares').innerHTML = '';
+  let days = document.createElement('div');
+  days.className = 'days';
+  document.getElementById('squares').appendChild(days);
