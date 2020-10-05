990482076842e6d5b2700366c55874beb4e39341
Lillyoverton <Lillyoverton1@gmail.com>
2020-10-05 17:45:40 +1300

    created database

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
+import sqlite3
+BIRTHDAYSDB = 'birthdays.db'
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/nzbirthdays', methods=['GET'])
+def nzbirthdays_list():
+    return jsonify('amount')
